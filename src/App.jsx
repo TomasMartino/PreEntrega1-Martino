feb0d328598792f784c06083142f82eab34702e8
TomasMartino <tomasdiamante882@gmail.com>
2025-02-15 18:18:02 -0300

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import NavBar from './components/NavBar'
+import './App.css'
+import './components/NavBar.jsx'
+
+function App() {
+ 
+
+  return (
+    <>
+     <NavBar/>
+    </>
+  )
+}
+
+export default App
